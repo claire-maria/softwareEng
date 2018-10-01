c8986f520eb4760d0465d15a937db3c0bf2dc7aa
Claire <34062660+claire-maria@users.noreply.github.com>
2018-10-01 12:21:42 +0100

    Add files via upload

--- LCATest.java
+++ LCATest.java
@@ -44,6 +44,18 @@ class LCATest {
 		 	tree.root.right.right = new Node(7);
 		 	System.out.println("LCA(3, 6): " + tree.findLCA(3,6));
 	    }
+	 @Test
+	    public void test4() {
+		 	LCA tree = new LCA();
+		 	tree.root = new Node(1); 
+		 	tree.root.left = new Node(2); 
+		 	tree.root.right = new Node(3); 
+		 	tree.root.left.left = new Node(4); 
+		 	tree.root.left.right = new Node(5); 
+		 	tree.root.right.left = new Node(6); 
+		 	tree.root.right.right = new Node(7);
+		 	System.out.println("LCA(3, 7): " + tree.findLCA(3,7));
+	    }
 	 
 
 }
