ee33bc0bada18c376e7e4c6b928a63d73971da9d
Claire <34062660+claire-maria@users.noreply.github.com>
2018-10-02 17:58:27 +0100

    Add files via upload

--- LCATest.java
+++ LCATest.java
@@ -92,7 +92,42 @@ class LCATest {
 		 	tree.root.right.right = new Node(7);
 		 	System.out.println("LCA(2, 4): " + tree.findLCA(2,4));
 	    }
-	 
+	 @Test
+	    public void test8() {
+		 	LCA tree = new LCA();
+		 	tree.root = new Node(1); 
+		 	tree.root.left = new Node(2); 
+		 	tree.root.right = new Node(3); 
+		 	tree.root.left.left = new Node(4); 
+		 	tree.root.left.right = new Node(5); 
+		 	tree.root.right.left = new Node(6); 
+		 	tree.root.right.right = new Node(7);
+		 	System.out.println("LCA(1, 4): " + tree.findLCA(1,4));
+	    }
+	 @Test
+	    public void test9() {
+		 	LCA tree = new LCA();
+		 	tree.root = new Node(1); 
+		 	tree.root.left = new Node(2); 
+		 	tree.root.right = new Node(3); 
+		 	tree.root.left.left = new Node(4); 
+		 	tree.root.left.right = new Node(5); 
+		 	tree.root.right.left = new Node(6); 
+		 	tree.root.right.right = new Node(7);
+		 	System.out.println("LCA(5, 2): " + tree.findLCA(5,2));
+	    }
+	 @Test
+	    public void test10() {
+		 	LCA tree = new LCA();
+		 	tree.root = new Node(1); 
+		 	tree.root.left = new Node(2); 
+		 	tree.root.right = new Node(3); 
+		 	tree.root.left.left = new Node(4); 
+		 	tree.root.left.right = new Node(5); 
+		 	tree.root.right.left = new Node(6); 
+		 	tree.root.right.right = new Node(7);
+		 	System.out.println("LCA(6, 7): " + tree.findLCA(6,7));
+	    }
 	 
 	 
 
