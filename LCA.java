70588d16d82f90f8d8ee88ab8369551561352baf
Claire <34062660+claire-maria@users.noreply.github.com>
2018-10-15 10:59:33 +0100

    Add files via upload

--- LCA.java
+++ LCA.java
@@ -1,83 +1,128 @@
 
 // Java Program for Lowest Common Ancestor in a Binary Tree 
 // A O(n) solution to find LCA of two given values n1 and n2 
-import java.util.ArrayList; 
-import java.util.List; 
-  
-// A Binary Tree node 
-class Node { 
-    int data; 
-    Node left, right; 
-  
-    Node(int value) { 
-        data = value; 
-        left = right = null; 
-    } 
-} 
-  
-public class LCA  
-{ 
-  
-    Node root; 
-    private List<Integer> path1 = new ArrayList<>(); 
-    private List<Integer> path2 = new ArrayList<>(); 
-  
-    // Finds the path from root node to given root of the tree. 
-    int findLCA(int n1, int n2) { 
-        path1.clear(); 
-        path2.clear(); 
-        return findLCAInternal(root, n1, n2); 
-    } 
-  
-    private int findLCAInternal(Node root, int n1, int n2) { 
-  
-        if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) { 
-            System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing"); 
-            System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing"); 
-            return -1; 
-        } 
-  
-        int i; 
-        for (i = 0; i < path1.size() && i < path2.size(); i++) { 
-              
-        // System.out.println(path1.get(i) + " " + path2.get(i)); 
-            if (!path1.get(i).equals(path2.get(i))) 
-                break; 
-        } 
-  
-        return path1.get(i-1); 
-    } 
-      
-    // Finds the path from root node to given root of the tree, Stores the 
-    // path in a vector path[], returns true if path exists otherwise false 
-    private boolean findPath(Node root, int n, List<Integer> path) 
-    { 
-        // base case 
-        if (root == null) { 
-            return false; 
-        } 
-          
-        // Store this node . The node will be removed if 
-        // not in path from root to n. 
-        path.add(root.data); 
-  
-        if (root.data == n) { 
-            return true; 
-        } 
-  
-        if (root.left != null && findPath(root.left, n, path)) { 
-            return true; 
-        } 
-  
-        if (root.right != null && findPath(root.right, n, path)) { 
-            return true; 
-        } 
-  
-        // If not present in subtree rooted with root, remove root from 
-        // path[] and return false 
-        path.remove(path.size()-1); 
-  
-        return false; 
-    } 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
 
-} 
+// A Binary Tree node 
+class Node {
+	int data;
+	Node left, right;
+
+	Node(int value) {
+		data = value;
+		left = right = null;
+	}
+}
+
+public class LCA {
+
+	Node root;
+	private List<Integer> path1 = new ArrayList<>();
+	private List<Integer> path2 = new ArrayList<>();
+
+	// Finds the path from root node to given root of the tree.
+	int findLCA(int n1, int n2) {
+		path1.clear();
+		path2.clear();
+		return findLCAInternal(root, n1, n2);
+	}
+
+	private int findLCAInternal(Node root, int n1, int n2) {
+
+		if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) {
+			System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing");
+			System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing");
+			return -1;
+		}
+
+		int i;
+		for (i = 0; i < path1.size() && i < path2.size(); i++) {
+
+			// System.out.println(path1.get(i) + " " + path2.get(i));
+			if (!path1.get(i).equals(path2.get(i)))
+				break;
+		}
+
+		return path1.get(i - 1);
+	}
+
+	// Finds the path from root node to given root of the tree, Stores the
+	// path in a vector path[], returns true if path exists otherwise false
+	private boolean findPath(Node root, int n, List<Integer> path) {
+		// base case
+		if (root == null) {
+			return false;
+		}
+
+		// Store this node . The node will be removed if
+		// not in path from root to n.
+		path.add(root.data);
+
+		if (root.data == n) {
+			return true;
+		}
+
+		if (root.left != null && findPath(root.left, n, path)) {
+			return true;
+		}
+
+		if (root.right != null && findPath(root.right, n, path)) {
+			return true;
+		}
+
+		// If not present in subtree rooted with root, remove root from
+		// path[] and return false
+		path.remove(path.size() - 1);
+
+		return false;
+	}
+
+	class Graph {
+		int V; // No. of vertices
+
+		List<Integer> adjListArray[];
+
+		public Graph(int V) {
+
+			this.V = V;
+
+			@SuppressWarnings("unchecked")
+			List<Integer> adjListArray[] = new LinkedList[V];
+
+			this.adjListArray = adjListArray;
+
+			for (int i = 0; i < V; i++) {
+				adjListArray[i] = new LinkedList<>();
+			}
+		}
+
+		// Utility function to add edge
+		public void addEdge(int src, int dest) {
+
+			this.adjListArray[src].add(dest);
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		// Create a graph given in the above diagram
+		Graph graph = new Graph(6);
+		graph.addEdge(5, 2);
+		graph.addEdge(5, 0);
+		graph.addEdge(4, 0);
+		graph.addEdge(4, 1);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 1);
+		System.out.println(graph);
+
+	}
+
+}
