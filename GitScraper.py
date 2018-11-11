84a6f99411680f8968c360b2a16882b790298fc9
Claire <34062660+claire-maria@users.noreply.github.com>
2018-11-11 16:19:07 +0000

    Add files via upload

--- GitScraper.py
+++ GitScraper.py
@@ -1,11 +1,32 @@
+from github3 import login, GitHub
+from getpass import getpass, getuser
 from github import Github
+import requests
+import json
+import sys
 
-print("enter your username")
-username = input()
-print("password")
-password = input()
-g = Github(username)
-user = g.get_user()
-print(user)
-for repo in g.get_user().get_repos():
-    print(repo.name)
+ACCESS_TOKEN = 'e1f2962d0b7b8475818f1689a711fade678f2e2'
+
+try:
+    import readline
+except ImportError:
+    pass
+
+try:
+    user = input('GitHub username: ')
+except KeyboardInterrupt:
+    user = getuser()
+password = getpass('GitHub password for {0}: '.format(user))
+
+
+
+def getRepoNames(user,password):
+    g = Github(user,password)
+    user = g.get_user()
+    print(user)
+    for repo in g.get_user().get_repos():
+        print(repo.name)
+        stargazers = [ s for s in repo.get_stargazers() ]
+        print("Number of stargazers", len(stargazers))
+
+getRepoNames(user,password)
